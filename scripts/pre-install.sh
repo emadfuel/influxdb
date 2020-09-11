21f674bb27976e4cfc130429200816c536fb3d29
Gershon Shif <gershon@influxdata.com>
2020-09-10 18:00:05 -0700

    chore: update install scripts

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 DATA_DIR=/var/lib/influxdb
+USER=influxdb
+GROUP=influxdb
 
 if ! id influxdb &>/dev/null; then
     useradd --system -U -M influxdb -s /bin/false -d $DATA_DIR
+fi
+
+# check if DATA_DIR exists
+if [ ! -d "$DATA_DIR" ]; then
+    mkdir -p $DATA_DIR
+    chown $USER:$GROUP $DATA_DIR
 fi
 
 if [[ -d /etc/opt/influxdb ]]; then
